7d9853db67bb044c37772c83426ae1ea51ab4996
diego <diegordesdecasa.es>
2021-04-24 12:29:12 +0200

    Linea 2

--- Prueba1Entornos/src/paquete/Principal.java
+++ Prueba1Entornos/src/paquete/Principal.java
@@ -5,8 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo. Java Git");
-		System.out.println("Hola mundo. Java Git testeo2");
-		System.out.println("Hola mundo. Java Git, testeo3");
+		
 	}
 
 }
