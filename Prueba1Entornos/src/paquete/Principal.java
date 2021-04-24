b11c5f8c0a72d7f843939c70430d473f9b36f9dc
diego <diegordesdecasa.es>
2021-04-24 12:30:39 +0200

    tercer commit, 3 lineas

--- Prueba1Entornos/src/paquete/Principal.java
+++ Prueba1Entornos/src/paquete/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo. Java Git");
-		
+		System.out.println("Hola mundo. Java Git, testeo2, segundo commit");
+		System.out.println("Hola mundo. Java Git, testeo3, tercer commit");
 	}
 
 }
